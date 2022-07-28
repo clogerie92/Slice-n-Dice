dd54d903b4c05a6ad3d034a59f3203c461c375bc
Ian Hill <ianhill@Ians-MBP.home>
2022-07-28 14:57:04 -0400

    adding server folder Typdefs and Resolvers

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,10 @@ export const Footer = () => {
         <div id = "footer">
             <p>Slice N' Dice Made With Love By:</p><br />
             <div id="icons">
-            <div><img src = "leo.png" height="75px" width="75px" /><p>clogerie92</p></div>
-            <div><img src = "mike.png" height="75px" width="75px" /><p>chrisjackson1</p></div>
-            <div><img src = "donatello.webp" height="75px" width="75px" /><p>krosario314</p></div>
-            <div><img src = "raph.png" height="75px" width="75px" /><p>ianahill</p></div>
+            <div id="leo"><img src = "leo.png" height="75px" width="75px" /><p>clogerie92</p></div>
+            <div id="mike"><img src = "mike.png" height="75px" width="75px" /><p>chrisjackson1</p></div>
+            <div id="don"><img src = "donatello.webp" height="75px" width="75px" /><p>krosario314</p></div>
+            <div id="raph"><img src = "raph.png" height="75px" width="75px" /><p>ianahill</p></div>
             </div>
         </div>
     )
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -10,8 +10,8 @@ export const NavBar = () => {
                 <img id="logo"src = "./logo.png" height="10%" width="10%" ></img>
                 <div id="mikey-quote"><p>“There comes a time, brothers, where history is forged like melted cheese… it sticks together as one, but it’s still soft and squishy in the middle… mmm… are you with me?!” -Michaelangelo</p></div>
                 <nav>
-                    <a>login</a>
-                    <a>signup</a>
+                    <a id="login">login</a>
+                    <a id="signup">signup</a>
                 </nav>
             </header>
         </div>
