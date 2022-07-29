69840689c79d31607a5ee71d09bad6adb3c8ed8a
Ian Hill <ianhill@Ians-MBP.home>
2022-07-29 10:02:42 -0400

    updating folder structure

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -2,7 +2,10 @@ import React, { useState } from "react";
 // import {Section} from "./section";
 import "./navBar.css"
 
-export const NavBar = () => {
+export const NavBar = ({testLogIn}) => {
+    const setLogIn = () => {
+        testLogIn(true);
+    }
     return (
         <div>
 
@@ -10,7 +13,7 @@ export const NavBar = () => {
                 <img id="logo"src = "./logo.png" height="10%" width="10%" ></img>
                 <div id="mikey-quote"><p>“There comes a time, brothers, where history is forged like melted cheese… it sticks together as one, but it’s still soft and squishy in the middle… mmm… are you with me?!” -Michaelangelo</p></div>
                 <nav>
-                    <a id="login">login</a>
+                    <a id="login" onClick = {setLogIn}>login</a>
                     <a id="signup">signup</a>
                 </nav>
             </header>
